b6a9f5879444082e23ca2558ebdf9610e3e7c119
iamsphere <ezekiel.9218@gmail.com>
2025-04-02 16:03:40 +0400

    Add all icons

---
+++ src/icons/Twitter.tsx
@@ -0,0 +1,13 @@
+import { FC, SVGProps } from 'react'
+
+export const Twitter: FC<SVGProps<SVGSVGElement>> = (props) => (
+  <svg width="18" height="18" viewBox="0 0 18 18" fill="none" {...props}>
+    <g id="Icon/x.com">
+      <path
+        id="Vector"
+        d="M13.683 1.6875H16.164L10.7438 7.8825L17.1203 16.3125H12.1275L8.21701 11.1997L3.74251 16.3125H1.26001L7.05751 9.68625L0.940506 1.6875H6.06001L9.59476 6.36075L13.683 1.6875ZM12.8123 14.8275H14.187L5.31301 3.0945H3.83776L12.8123 14.8275Z"
+        fill="#8C8C8C"
+      />
+    </g>
+  </svg>
+)
